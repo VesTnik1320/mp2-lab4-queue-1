7790f71c28e3394f836d831dd7fbf582d9271cb5
Ferikus <65133455+Ferikus@users.noreply.github.com>
2023-12-29 03:45:16 +0300

    queue list remake

--- Queue/Queue.h
+++ Queue/Queue.h
@@ -1,58 +1,90 @@
 #pragma once
 
 template <class T>
+struct TNode {
+	T val;
+	TNode* pNext = nullptr;
+};
+
+template <class T>
 class TQueue
 {
-	T* pMem;
-	int size, start, finish, count;
+	TNode<T>* pFirst, * pLast;
 public:
-	TQueue(int _size) {
-		size = _size;
-		pMem = new T[size];
-		count = 0;
-		start = 0;
-		finish = -1;
+	TQueue() {
+		pFirst = nullptr;
+		pLast = nullptr;
+	}
+
+	TQueue(const TQueue& q) {
+		TNode<T>* tmp = q.pFirst, * i;
+		pFirst = pLast = nullptr;
+		while (tmp != nullptr) {
+			i = new TNode<T>;
+			i->val = tmp->val;
+			if (pFirst == nullptr) pFirst = pLast = i;
+			else {
+				pLast->pNext = i;
+				pLast = i;
+			}
+			tmp = tmp->pNext;
+		}
+	}
+
+	~TQueue() {
+		//пройтись по очереди и освободить все €чейки
+		TNode<T>* i;
+		while (pFirst != pLast) {
+			pFirst = pFirst->pNext;
+		}
+		pFirst = pLast = nullptr;
 	}
 
 	void push(const T& el) {
 		if (full()) throw "The queue is full";
-		finish++;
-		finish %= size; //if (finish == size) finish = 0;
-		pMem[finish] = el;
-		count++;
+		TNode<T>* tmp = new TNode<T>;
+		tmp->val = el;
+		if (empty()) pFirst = pLast = tmp;
+		else {
+			pLast->pNext = tmp;
+			pLast = tmp;
+		}
 	}
 
 	T pop() {
 		if (empty()) throw "The queue is empty";
-		T el = pMem[start];
-		start++;
-		start %= size; // if (start == size) start = 0;
-		count--;
+		TNode<T>* tmp = pFirst;
+		T el = pFirst->val;
+		pFirst = pFirst->pNext;
+		//delete tmp;
 		return el;
+		//когда 1 элемент?
 	}
 
-	bool empty() { return count == 0; }
+	bool empty() { return pFirst == nullptr; }
 
-	bool full() { return count == size; }
-
-	T front() {
-		if (empty()) throw "The queue is empty";
-		return pMem[start];
+	bool full() {
+		TNode<T>* p = new TNode<T>;
+		bool res = (p == nullptr);
+		delete p;
+		return res;
 	}
 
-	T back() {
-		if (empty()) throw "The queue is empty";
-		return pMem[finish];
-	}
+	T front() { return pFirst->val; }
+
+	T back() { return pLast->val; }
 
 	bool operator==(const TQueue& q) {
-		if (size != q.size) return false;
-		else if (count != q.count) return false;
-		else if (start != q.start) return false;
-		else {
-			for (int i = 0; i++; i < count) {
-				if (pMem[i] != q.pMem[i]) return false;
+		TNode<T>* pRight = q.pFirst, * pLeft = pFirst;
+		while (pRight != nullptr || pLeft != nullptr) {
+			if (pRight->val != pLeft->val) {
+				return false;
 			}
+			pRight = pRight->pNext;
+			pLeft = pLeft->pNext;
+		}
+		if ((pRight == nullptr && pLeft != nullptr) || (pLeft == nullptr && pRight != nullptr)) {
+			return false;
 		}
 		return true;
 	}
@@ -60,10 +92,4 @@ public:
 	bool operator!=(const TQueue& q) {
 		return !(*this == q);
 	}
-
-	int getMaxSize() { return size; }
-
-	int getSize() { return count; }
-
-	int getHead() { return start; }
 };
--- gtest/test_tqueue.cpp
+++ gtest/test_tqueue.cpp
@@ -3,37 +3,25 @@
 
 TEST(TQueue, CAN_CREATE_POSITIVE_QUEUE)
 {
-    ASSERT_NO_THROW(TQueue<int> q(3));
-}
-
-TEST(TQueue, CANT_CREATE_NEGATIVE_QUEUE)
-{
-    ASSERT_ANY_THROW(TQueue<int> q(-5));
-}
-
-TEST(TQueue, CAN_GET_SIZE)
-{
-    TQueue<int> q(5);
-
-    EXPECT_EQ(0, q.getSize());
+    ASSERT_NO_THROW(TQueue<int> q);
 }
 
 TEST(TQueue, NEW_QUEUE_IS_EMPTY)
 {
-    TQueue<int> q(5);
+    TQueue<int> q;
 
     ASSERT_TRUE(q.empty());
 }
 
 TEST(TQueue, CAN_PUSH)
 {
-    TQueue<int> q(5);
+    TQueue<int> q;
     ASSERT_NO_THROW(q.push(1));
 }
 
 TEST(TQueue, CAN_POP)
 {
-    TQueue<int> q(5);
+    TQueue<int> q;
     int a;
     q.push(1);
     ASSERT_NO_THROW(a = q.pop());
@@ -42,21 +30,13 @@ TEST(TQueue, CAN_POP)
 
 TEST(TQueue, POP_ON_EMPTY_QUEUE_THROWS)
 {
-    TQueue<int> q(5);
+    TQueue<int> q;
     ASSERT_ANY_THROW(q.pop());
-}
-
-TEST(TQueue, PUSH_ON_FULL_QUEUE_THROWS)
-{
-    TQueue<int> q(2);
-    q.push(1);
-    q.push(1);
-    ASSERT_ANY_THROW(q.push(1));
 }
 
 TEST(TQueue, CAN_FRONT_OR_BACK)
 {
-    TQueue<int> q(2);
+    TQueue<int> q;
     q.push(1); q.push(1);
 
     ASSERT_NO_THROW(q.front());
@@ -66,23 +46,15 @@ TEST(TQueue, CAN_FRONT_OR_BACK)
 
 TEST(TQueue, FRONT_OR_BACK_ON_EMPTY_QUEUE_THROWS)
 {
-    TQueue<int> q(3);
+    TQueue<int> q;
 
     ASSERT_ANY_THROW(q.front());
     ASSERT_ANY_THROW(q.back());
 }
 
-TEST(TQueue, QUEUE_IS_FULL)
-{
-    TQueue<int> q(3);
-    q.push(1); q.push(1); q.push(1);
-
-    ASSERT_TRUE(q.full());
-}
-
 TEST(TQueue, QUEUE_IS_NOT_FULL)
 {
-    TQueue<int> q(3);
+    TQueue<int> q;
     q.push(1);
 
     ASSERT_FALSE(q.full());
@@ -90,14 +62,14 @@ TEST(TQueue, QUEUE_IS_NOT_FULL)
 
 TEST(TQueue, QUEUE_IS_EMPTY)
 {
-    TQueue<int> q(3);
+    TQueue<int> q;
 
     ASSERT_TRUE(q.empty());
 }
 
 TEST(TQueue, QUEUE_IS_NOT_EMPTY)
 {
-    TQueue<int> q(3);
+    TQueue<int> q;
     q.push(1);
 
     ASSERT_FALSE(q.empty());
@@ -105,7 +77,7 @@ TEST(TQueue, QUEUE_IS_NOT_EMPTY)
 
 TEST(TQueue, COMPARE_TWO_EQUAL_QUEUES)
 {
-    TQueue<int> q1(3), q2(3);
+    TQueue<int> q1, q2;
     q1.push(1); q1.push(1);
     q2.push(1); q2.push(1);
 
@@ -114,7 +86,7 @@ TEST(TQueue, COMPARE_TWO_EQUAL_QUEUES)
 
 TEST(TQueue, COMPARE_TWO_DIFFERENT_QUEUES)
 {
-    TQueue<int> q1(3), q2(3);
+    TQueue<int> q1, q2;
     q1.push(0); q1.push(0);
     q2.push(1); q2.push(1);
 
